5c16e2c9fe7cdce1991d9018c4213c2fe17f2313
Thabung <thabungm@gmail.com>
2018-05-21 08:19:23 +0530

    First commit

--- index.js
+++ index.js
@@ -1,18 +1,45 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var app = require("express")();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
 var port = process.env.PORT || 3000;
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+var request = require("request");
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/bot.png");
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+
+botIcon = '<img src="http://www.bitrebels.com/wp-content/uploads/2011/02/Original-Facebook-Geek-Profile-Avatar-1.jpg" class=" img-responsive ">';
+userIcon = ''
+http://www.bitrebels.com/wp-content/uploads/2011/02/Original-Facebook-Geek-Profile-Avatar-2.jpg
+io.on("connection", function(socket) {
+  socket.on("chat message", function(msg) {
+    io.emit("chat message", "<div >You: " + msg + "</div>");
+    // var json_obj = JSON.parse(data);
+
+    setTimeout(() => {
+      request.post(
+        {
+          headers: { "content-type": "application/json" },
+          url: "http://localhost:8080",
+          form: JSON.stringify({
+            query: msg
+          })
+        },
+        function(error, response, body) {
+          body = JSON.parse(body);
+          console.log(body["result"]);
+          io.emit("chat message", "Bot: " + body["result"]);
+        }
+      );
+    }, 1000);
   });
 });
 
-http.listen(port, function(){
-  console.log('listening on *:' + port);
+http.listen(port, function() {
+  console.log("listening on *:" + port);
 });
